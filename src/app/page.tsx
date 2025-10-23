c773a3df83eec08982c9f9e3ee88c432cd275be9
BeukerDipsy <guus.nohlmans@gmail.com>
2025-10-23 18:13:56 +0200

    Added section scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,24 @@
 import GuusNohlmans from "@/components/customName/guusNohlmans";
 
 export default function Home() {
-  
-  
+  const scrollToSection = () => {
+    const aboutMeSection = document.getElementById("aboutMe");
+    if (aboutMeSection) {
+      aboutMeSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <>
-    <div className="flex flex-col items-center justify-center min-h-screen max-w-[1440px] w-full mx-auto">
-      <GuusNohlmans/>
-      <h2 className="text-4xl -mt-6 text-secondaryYellow">turning creative ideas into creative applications</h2>
-    </div>
-    <section id="aboutMe"> {/** About Me section **/}
+    {/** Hero section **/}
+      <section id="hero"> 
+        <div className="flex flex-col items-center justify-center min-h-screen max-w-[1440px] w-full mx-auto">
+          <GuusNohlmans onClick={scrollToSection} />
+          <h2 className="text-4xl -mt-6 text-secondaryYellow">turning creative ideas into creative applications</h2>
+        </div>
+      </section>
+
+      {/** About Me section **/}
+      <section id="aboutMe"> 
         <div className="flex flex-col items-center justify-center min-h-screen max-w-[1440px] w-full mx-auto">
           <h2 className="text-[2.25rem] text-secondaryYellow">About Me</h2>
         </div>
--- src/components/customName/guusNohlmans.tsx
+++ src/components/customName/guusNohlmans.tsx
@@ -1,9 +1,9 @@
 import DipsySvg from "./dipsySvg";
 import GrowingLetters from "./growingLetters";
 
-export default function GuusNohlmans() {
+export default function GuusNohlmans( { onClick }: { onClick: () => void }) {
   return (
-    <div className="flex justify-center text-[9rem] font-bold cursor-pointer gap-8  hover:scale-102 transition" > {/** Move naar Guus nohlmans geedeelte **/}
+    <div className="flex justify-center text-[9rem] font-bold cursor-pointer gap-8  hover:scale-102 transition" onClick={onClick}> {/** Move naar Guus nohlmans geedeelte **/}
       <GrowingLetters text="Guus" />
       <div className="flex">
         <GrowingLetters text="N" />
