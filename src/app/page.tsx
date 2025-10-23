d4c3b392fa652662be497df2961f32964a3322ad
BeukerDipsy <guus.nohlmans@gmail.com>
2025-10-23 18:12:28 +0200

    Added about me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
+"use client"
 import GuusNohlmans from "@/components/customName/guusNohlmans";
 
 export default function Home() {
+  
+  
   return (
     <>
     <div className="flex flex-col items-center justify-center min-h-screen max-w-[1440px] w-full mx-auto">
-      <GuusNohlmans />
+      <GuusNohlmans/>
       <h2 className="text-4xl -mt-6 text-secondaryYellow">turning creative ideas into creative applications</h2>
     </div>
+    <section id="aboutMe"> {/** About Me section **/}
+        <div className="flex flex-col items-center justify-center min-h-screen max-w-[1440px] w-full mx-auto">
+          <h2 className="text-[2.25rem] text-secondaryYellow">About Me</h2>
+        </div>
+      </section>
     </>
   );
 }
--- src/components/customName/guusNohlmans.tsx
+++ src/components/customName/guusNohlmans.tsx
@@ -3,7 +3,7 @@ import GrowingLetters from "./growingLetters";
 
 export default function GuusNohlmans() {
   return (
-    <div className="flex justify-center text-[9rem] font-bold cursor-pointer gap-8  hover:scale-102 transition"> {/** Move naar Guus nohlmans geedeelte **/}
+    <div className="flex justify-center text-[9rem] font-bold cursor-pointer gap-8  hover:scale-102 transition" > {/** Move naar Guus nohlmans geedeelte **/}
       <GrowingLetters text="Guus" />
       <div className="flex">
         <GrowingLetters text="N" />
