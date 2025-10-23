6b7b37c53691f08041aa99ce31193a4847affe12
BeukerDipsy <guus.nohlmans@gmail.com>
2025-10-23 18:07:26 +0200

    Fixed hero margins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import GuusNohlmans from "@/components/customName/guusNohlmans";
 export default function Home() {
   return (
     <>
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 max-w-[1440px] w-full mx-auto">
+    <div className="flex flex-col items-center justify-center min-h-screen max-w-[1440px] w-full mx-auto">
       <GuusNohlmans />
-      <h2 className="text-4xl mt-4 mb-8 text-secondaryYellow">turning creative ideas into creative applications</h2>
+      <h2 className="text-4xl -mt-6 text-secondaryYellow">turning creative ideas into creative applications</h2>
     </div>
     </>
   );
