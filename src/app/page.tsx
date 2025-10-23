5c5840334aa753e747620f3b8c85afe049f46442
BeukerDipsy <guus.nohlmans@gmail.com>
2025-10-24 00:46:05 +0200

    Fixed quote

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
+"use client";
 import GuusNohlmans from "@/components/customName/guusNohlmans";
 
 export default function Home() {
 
-  const scrollToSection = (sectionId: string) => {
-    const section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
-    }
-  };
+  // const scrollToSection = (sectionId: string) => {
+  //   const section = document.getElementById(sectionId);
+  //   if (section) {
+  //     section.scrollIntoView({ behavior: "smooth" });
+  //   }
+  // };
   
   return (
     <>
     <div className="flex flex-col items-center justify-center min-h-screen py-2 max-w-[1440px] w-full mx-auto">
       <GuusNohlmans />
-      <h2 className="text-4xl mt-4 mb-8 text-secondaryYellow">turning creative ideas into creative applications</h2>
+      <h2 className="text-4xl mt-4 mb-8 text-secondaryYellow">Turning creative ideas into creative applications.</h2>
     </div>
     </>
   );
