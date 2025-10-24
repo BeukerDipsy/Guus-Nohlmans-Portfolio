03adaf7ed7481847401c53eae1c0107d469fd3aa
BeukerDipsy <guus.nohlmans@gmail.com>
2025-10-24 14:54:47 +0200

    Fixd spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,19 +27,22 @@ export default function Home() {
         </section>
 
         {/** About Me section **/}
-        <section id="aboutMe" className="py-8 md:py-16">
-          <div className="flex flex-col md:flex-row md:justify-between min-h-screen font-bold w-full p-4">
+        <section
+          id="aboutMe"
+          className="max-h-screen h-screen overflow-hidden flex items-center"
+        >
+          <div className="flex flex-col md:flex-row md:justify-between h-full font-bold w-full p-4 items-center ">
             <div className="flex flex-col flex-wrap pr-0 md:pr-10 lg:pr-20 ">
               <h1 className="text-[4rem] md:text-[6rem] [text-shadow:_0px_5px_2px_rgb(0_0_0_/_0.18)] text-left">
                 About me
               </h1>
-              <p className="text-[1.5rem] md:text-[1.7rem] pt-8 md:pt-16  md:pr-0">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus iaculis augue eu blandit accumsan. Quisque imperdiet nisi justo. Donec metus augue, tristique eget auctor eu, mattis ac arcu. Mauris consectetur turpis et risus rhoncus tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque imperdiet nisi justo.
-              </p>
+              <p className="text-[1.5rem] md:text-[1.7rem] pt-8 md:pt-16 md:pr-0"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus iaculis augue eu blandit accumsan. Quisque imperdiet nisi justo. Donec metus augue, tristique eget auctor eu, mattis ac arcu. Mauris consectetur turpis et risus rhoncus tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque imperdiet nisi justo. </p>
             </div>
+
             <DipsyFace className="pt-10 md:pt-17 self-center md:self-auto scale-70 xs:scale-75 sm:scale-80 md:scale-85 lg:scale-90 xl:scale-95 2xl:scale-100" />
           </div>
         </section>
+
 
         {/** Skills section **/}
         <section id="skills">
