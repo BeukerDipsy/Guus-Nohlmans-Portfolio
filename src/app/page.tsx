b13926349c3a5d770bd79b465e0bef6f570f00fa
BeukerDipsy <guus.nohlmans@gmail.com>
2025-10-28 16:54:49 +0100

    Fixed sizing and text-color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
         <section id="projects">
           <div className="flex flex-col items-center justify-center mx-auto w-full px-4">
             <h2 className="text-[1.75rem] md:text-[2.25rem] text-secondaryYellow text-center">Projects</h2>
-            <div className="flex flex-row w-full justify-between">
+            <div className="flex flex-row w-full justify-between space-x-[1.5rem]">
               <ProjectCard title="Project 1" description="Description for project 1" imagePath="/images/Sabroso.svg" />
               <ProjectCard title="Project 3" description="Description for project 3" imagePath="/images/Actipulse.svg" />
               <ProjectCard title="Project 2" description="Description for project 2" imagePath="/images/WiebelToren.svg" />
--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -1,13 +1,11 @@
+import Image from "next/image";
+
 export default function ProjectCard({ title, description, imagePath }: { title: string; description: string; imagePath: string }) {
   return (
-    <div className="bg-primaryYellow rounded-2xl w-[27rem] h-[27.7rem] flex flex-col justify-between p-[0.6rem]">
-      <div className="bg-auto w-full h-[12.3rem] outline-3 rounded-xl bg-cover bg-center" style={{ backgroundImage: `url(${imagePath})` }}>
-
-      </div>
-      <div className="flex flex-col p-4">
-        <h3 className="text-xl font-bold">{title}</h3>
-        <p className="text-sm">{description}</p>
-      </div>
+    <div className="bg-primaryYellow rounded-2xl w-[27rem] h-[31.5rem] flex flex-col p-[1rem]">
+      <Image src={imagePath} alt={title} width={500} height={300} className="rounded-xl" />
+      <h3 className="text-xl font-bold text-secondaryGreen">{title}</h3>
+      <p className="text-sm text-secondaryGreen">{description}</p>
     </div>
   )
 }
