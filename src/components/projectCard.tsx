dfc6ca36dabb967665b9bc33aad7d9a98f2d9b41
BeukerDipsy <guus.nohlmans@gmail.com>
2025-10-28 17:06:08 +0100

    Fixed title and description sizing

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -4,8 +4,11 @@ export default function ProjectCard({ title, description, imagePath }: { title: 
   return (
     <div className="bg-primaryYellow rounded-2xl w-[27rem] h-[31.5rem] flex flex-col p-[1rem]">
       <Image src={imagePath} alt={title} width={500} height={300} className="rounded-xl" />
-      <h3 className="text-xl font-bold text-secondaryGreen">{title}</h3>
-      <p className="text-sm text-secondaryGreen">{description}</p>
+      <div className="flex flex-col mt-4 space-y-2">
+        <h3 className="text-[1.7rem] font-extrabold text-secondaryGreen">{title}</h3>
+      <p className="text-[1.1rem] text-secondaryGreen">{description}</p>
+      </div>
+      
     </div>
   )
 }
