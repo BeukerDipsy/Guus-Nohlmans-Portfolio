bd6dbd4cfbf4db96910af10b25c728e37197f05d
Nohlmans,Guus G. <guus.nohlmans@gmail.com>
2025-10-29 14:09:18 +0100

    Fixed custom styling and added status messages

--- src/components/ContactBox/contactBox.tsx
+++ src/components/ContactBox/contactBox.tsx
@@ -13,14 +13,12 @@ export default function ContactBox() {
       <div className="w-[68%] p-8 rounded-2xl border bg-primaryYellow flex flex-col gap-4 shadow-[1rem_1rem_0_rgba(0,0,0,0.18)]">
         <EmailForm />
       </div>
-     
 
-      {/** Components per box **/ }
-  <div className="pl-10 space-y-6 flex flex-col w-[32%]">
-    <InfoBox title="Email" description="example.mail@gmail.com" />
-    <InfoBox title="Phone" description="+31 6 12345678" />
-    <InfoBox title="Availability" description="Available" />
-  </div>
+      <div className="pl-10 space-y-6 flex flex-col w-[32%]">
+        <InfoBox title="Email" description="example.mail@gmail.com" />
+        <InfoBox title="Phone" description="+31 6 12345678" />
+        <InfoBox title="Availability" description="Available" />
+      </div>
 
     </div >
   );
