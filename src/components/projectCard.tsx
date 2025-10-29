9d10d7373c66b40992dba4b20d7797251d786646
Nohlmans,Guus G. <guus.nohlmans@gmail.com>
2025-10-29 15:35:10 +0100

    Fixed warnings

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -3,7 +3,7 @@ import ViewDetailsButton from "./viewDetailsButton";
 
 export default function ProjectCard({ title, description, imagePath }: { title: string; description: string; imagePath: string }) {
   return (
-    <div className="bg-primaryYellow rounded-2xl w-[27rem] h-[31.5rem] flex flex-col p-[1rem] shadow-[0.8rem_0.8rem_0_rgba(0,0,0,0.18)]">
+    <div className="bg-primaryYellow rounded-2xl w-108 h-126 flex flex-col p-4 shadow-[0.8rem_0.8rem_0_rgba(0,0,0,0.18)]">
       <Image src={imagePath} alt={title} width={500} height={300} className="rounded-xl hover" />
       <div className="flex flex-col mt-4 space-y-2">
         <h3 className="text-[1.7rem] font-extrabold text-secondaryGreen">{title}</h3>
