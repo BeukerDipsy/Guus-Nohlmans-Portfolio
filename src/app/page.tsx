4e78fdd821660384110c6c198a9ffcddf3741621
Nohlmans,Guus G. <guus.nohlmans@gmail.com>
2025-10-29 15:38:01 +0100

    Fixed warnings on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const scrollToSection = (sectionId: string) => {
     const section = document.getElementById(sectionId);
     if (section) {
-      section.scrollIntoView({ behavior: "smooth", block: "center"  });
+      section.scrollIntoView({ behavior: "smooth", block: "center" });
     }
   };
 
@@ -65,16 +65,8 @@ export default function Home() {
         {/** Projects section **/}
         <section id="projects" className="pb-[10vh]">
           <div className="flex flex-col justify-center mx-auto w-full px-4">
-            <h1 className="text-[4rem] md:text-[6rem] [text-shadow:0.4rem_0.4rem_0rem_rgb(0_0_0_/0.18)] text-secondaryYellow font-bold text-left mb-[1rem]">Projects</h1>
-            <div className="flex flex-row w-full justify-between space-x-[1.5rem]">
-              <ProjectCard title="Sabroso" description="Developing a smart restaurant ordering system. Orders are instantly sent to the kitchen or bar, streamlining the workflow and improving the overall dining experience." imagePath="/images/Sabroso.svg" />
-              <ProjectCard title="Actipulse" description="Developing an application/device to improve productivity and health by tracking sitting-habits." imagePath="/images/Actipulse.svg" />
-              <ProjectCard title="WiebelToren" description="Designing and developing a game for the less-abled community, played with the Pillo from Hulan." imagePath="/images/WiebelToren.svg" />
-            </div>
-        <section id="projects" className="pb-[10vh]">
-          <div className="flex flex-col justify-center mx-auto w-full px-4">
-            <h1 className="text-[4rem] md:text-[6rem] [text-shadow:0.4rem_0.4rem_0rem_rgb(0_0_0_/0.18)] text-secondaryYellow font-bold text-left mb-[1rem]">Projects</h1>
-            <div className="flex flex-row w-full justify-between space-x-[1.5rem]">
+            <h1 className="text-[4rem] md:text-[6rem] [text-shadow:0.4rem_0.4rem_0rem_rgb(0_0_0_/0.18)] text-secondaryYellow font-bold text-left mb-4">Projects</h1>
+            <div className="flex flex-row w-full justify-between space-x-6">
               <ProjectCard title="Sabroso" description="Developing a smart restaurant ordering system. Orders are instantly sent to the kitchen or bar, streamlining the workflow and improving the overall dining experience." imagePath="/images/Sabroso.svg" />
               <ProjectCard title="Actipulse" description="Developing an application/device to improve productivity and health by tracking sitting-habits." imagePath="/images/Actipulse.svg" />
               <ProjectCard title="WiebelToren" description="Designing and developing a game for the less-abled community, played with the Pillo from Hulan." imagePath="/images/WiebelToren.svg" />
