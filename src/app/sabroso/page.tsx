84b79d4f228a18c28a6edeb37f3f8bd7ccd74a36
BeukerDipsy <guus.nohlmans@gmail.com>
2025-11-11 21:51:21 +0100

    Added title description and image

--- src/app/sabroso/page.tsx
+++ src/app/sabroso/page.tsx
@@ -1,13 +1,17 @@
+import Project from "@/components/project";
+
 export default function SabrosoPage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-primaryGreen">
-      <h1 className="text-4xl font-bold mb-4">Sabroso Project Page</h1>
-      {/* <p className="text-lg text-center max-w-2xl px-4">
-        Sabroso is an innovative restaurant ordering system designed to streamline the dining experience. With Sabroso, customers can easily browse the menu, place orders, and customize their meals directly from their smartphones or tablets. The system instantly sends orders to the kitchen or bar, improving workflow efficiency and reducing wait times. Sabroso also offers features like real-time order tracking, secure payment options, and personalized recommendations based on customer preferences. Whether you're a diner looking for convenience or a restaurant aiming to enhance service quality, Sabroso is the perfect solution for modern dining.
-      </p> */}
-      <p>
-        under construction...
-      </p>
-    </div>
+    // <div className="min-h-screen flex flex-col items-center justify-center bg-primaryGreen">
+    //   <h1 className="text-4xl font-bold mb-4">Sabroso Project Page</h1>
+    //   {/* <p className="text-lg text-center max-w-2xl px-4">
+    //     Sabroso is an innovative restaurant ordering system designed to streamline the dining experience. With Sabroso, customers can easily browse the menu, place orders, and customize their meals directly from their smartphones or tablets. The system instantly sends orders to the kitchen or bar, improving workflow efficiency and reducing wait times. Sabroso also offers features like real-time order tracking, secure payment options, and personalized recommendations based on customer preferences. Whether you're a diner looking for convenience or a restaurant aiming to enhance service quality, Sabroso is the perfect solution for modern dining.
+    //   </p> */}
+    //   <p>
+    //     under construction...
+    //   </p>
+    // </div>
+
+    <Project title="Sabroso" description="Developing a smart restaurant ordering system"></Project>
   );
 }
