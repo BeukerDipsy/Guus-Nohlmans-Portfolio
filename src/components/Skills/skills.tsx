ac4a12f882490ac2c179fd2863a891451bee8188
Nohlmans,Guus G. <guus.nohlmans@gmail.com>
2025-11-16 00:28:29 +0100

    Added partially Responsiveness of skills section
    Education sort of finished, look at the dots height and total size...

--- src/components/Skills/skills.tsx
+++ src/components/Skills/skills.tsx
@@ -1,12 +1,12 @@
 export default function Skills() {
   return (
-    <div className="px-8 py-6 rounded-2xl w-full bg-primaryYellow shadow-[0.8rem_0.8rem_0_rgba(0,0,0,0.12)] items-start text-primaryGreen h-full">
+    <div className="px-8 py-6 rounded-2xl min-w-[33vw] max-w-[40vw] bg-primaryYellow shadow-[0.8rem_0.8rem_0_rgba(0,0,0,0.12)] items-start text-primaryGreen h-full">
       <h1 className="text-[3.15rem] [text-shadow:0.23rem_0.23rem_0rem_rgb(0_0_0_/0.18)] font-bold">Skills</h1>
       <hr className="border-2 my-1 rounded-full" />
       <div className="mt-4 flex flex-col gap-4">
-        <div className="w-full">
+        <div className="">
           <h2 className="text-[1.8rem] font-semibold">Front-end</h2>
-          <div className="grid grid-cols-2 gap-4 text-[1.44rem]">
+          <div className="grid grid-cols-2 gap-4 text-[1.44rem] ">
             <ul className="list-disc list-inside">
               <li>HTML</li>
               <li>CSS/Tailwind</li>
@@ -18,14 +18,14 @@ export default function Skills() {
               <li>Figma</li>
             </ul>
           </div>
-          <hr className="border-1 my-4 rounded-full" />
+          <hr className="border my-4 rounded-full" />
           <h2 className="text-[1.8rem] font-semibold">Back-end</h2>
           <ul className="list-disc list-inside text-[1.44rem]">
             <li>C#/ASP.NET Core</li>
             <li>RESTful API Development</li>
             <li>Node.js</li>
           </ul>
-          <hr className="border-1 my-4 rounded-full" />
+          <hr className="border my-4 rounded-full" />
           <h2 className="text-[1.8rem] font-semibold">Data Management</h2>
           <div className="grid grid-cols-2 gap-2 text-[1.44rem]">
             <li className="list-disc list-inside">SQL</li>
