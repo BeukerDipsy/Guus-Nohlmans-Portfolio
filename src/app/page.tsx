e66754d7187538f427c145dc0a5acb50c3cec8db
Nohlmans,Guus G. <guus.nohlmans@gmail.com>
2025-12-02 16:56:23 +0100

    Fixed skills spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
         <section id="experience" className="pb-[10vh] md:mx-0 mx-10">
           <div className="flex flex-col justify-center mx-auto w-full px-4">
             <h1 className="text-[10rem] md:text-[8rem] lg:text-[6rem] [text-shadow:0.67rem_0.67rem_0rem_rgb(0_0_0_/0.18)] md:[text-shadow:0.53rem_0.53rem_0rem_rgb(0_0_0_/0.18)] lg:[text-shadow:0.4rem_0.4rem_0rem_rgb(0_0_0_/0.18)] text-secondaryYellow font-bold text-left">Experience</h1>
-            <div className="flex-col md:flex-row flex justify-between w-full mt-2 space-x-1 space-y-15">
+            <div className="flex-col md:flex-row flex w-full mt-2 gap-10 space-y-15">
               <Education/>
               <Skills/>
             </div>
@@ -86,7 +86,7 @@ export default function Home() {
         {/** Contact section **/}
         <section id="contact" className=" pb-[5vh] md:pb-[17vh] md:mx-0 mx-10">
           <div className="flex flex-col justify-center w-full mx-auto">
-            <h1 className=" md:mb-0 mb-[2rem] text-[10rem] md:text-[8rem] lg:text-[6rem] [text-shadow:0.67rem_0.67rem_0rem_rgb(0_0_0_/0.18)] md:[text-shadow:0.53rem_0.53rem_0rem_rgb(0_0_0_/0.18)] lg:[text-shadow:0.4rem_0.4rem_0rem_rgb(0_0_0_/0.18)] text-secondaryYellow font-bold text-left">Contact me</h1>
+            <h1 className=" md:mb-0 mb-8 text-[10rem] md:text-[8rem] lg:text-[6rem] [text-shadow:0.67rem_0.67rem_0rem_rgb(0_0_0_/0.18)] md:[text-shadow:0.53rem_0.53rem_0rem_rgb(0_0_0_/0.18)] lg:[text-shadow:0.4rem_0.4rem_0rem_rgb(0_0_0_/0.18)] text-secondaryYellow font-bold text-left">Contact me</h1>
             <ContactBox />
           </div>
         </section>
--- src/components/Skills/skills.tsx
+++ src/components/Skills/skills.tsx
@@ -1,6 +1,6 @@
 export default function Skills() {
   return (
-    <div className="px-8 py-6 rounded-2xl min-w-[30vw] mx-[7rem] md:mx-0 md:max-w-[40vw] bg-primaryYellow shadow-[0.8rem_0.8rem_0_rgba(0,0,0,0.12)] items-start h-full text-primaryGreen">
+    <div className="px-8 py-6 rounded-2xl min-w-[30vw] md:mx-0 mx-[7rem] md:max-w-[20vw] w-auto bg-primaryYellow shadow-[0.8rem_0.8rem_0_rgba(0,0,0,0.12)] items-start h-full text-primaryGreen">
       <h1 className="text-[6.3rem] md:text-[3.15rem] [text-shadow:0.25rem_0.25rem_0rem_rgb(0_0_0_/0.18)] font-bold">Skills</h1>
       <hr className="border-2 my-1 rounded-full" />
       <div className="mt-4 flex flex-col gap-4">
