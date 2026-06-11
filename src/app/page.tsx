66bc559ba94054ae01655d925f13ff861f209d01
BeukerDipsy <guus.nohlmans@gmail.com>
2026-06-11 15:35:37 +0200

    Reworked project section on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,9 +73,9 @@ export default function Home() {
           <div className="flex flex-col md:flex-row md:justify-between h-full font-bold w-full px-4">
             <div className="flex flex-col flex-wrap pr-0 md:pr-10 lg:pr-20 ">
               <h1 className="text-[10rem] md:text-[8rem] lg:text-[6rem] [text-shadow:0.67rem_0.67rem_0rem_rgb(0_0_0_/0.18)] md:[text-shadow:0.53rem_0.53rem_0rem_rgb(0_0_0_/0.18)] lg:[text-shadow:0.4rem_0.4rem_0rem_rgb(0_0_0_/0.18)] text-secondaryYellow font-bold text-left">About me</h1>
-              <p className="text-[3rem] md:text-[2.35rem] lg:text-[1.7rem] pt-8 md:pt-16 md:pr-0 font-medium"> I&apos;m Guus, a student at Fontys University of Applied Sciences with a strong passion for development and technology. 
-                <br/> <br/>
-                I love bringing ideas to life through code and finding unique approaches to tackle problems. 
+              <p className="text-[3rem] md:text-[2.35rem] lg:text-[1.7rem] pt-8 md:pt-16 md:pr-0 font-medium"> I&apos;m Guus, a student at Fontys University of Applied Sciences with a strong passion for development and technology.
+                <br /> <br />
+                I love bringing ideas to life through code and finding unique approaches to tackle problems.
                 <br className="py-1" />
                 I enjoy working with others and can easily lose track of time when working on a project I am passionate about. In my free time, I go to my local scouting, spend time with friends and work on personal projects. </p>
             </div>
@@ -96,8 +96,8 @@ export default function Home() {
           <div className="flex flex-col justify-center mx-auto w-full px-4">
             <h1 className="text-[10rem] md:text-[8rem] lg:text-[6rem] [text-shadow:0.67rem_0.67rem_0rem_rgb(0_0_0_/0.18)] md:[text-shadow:0.53rem_0.53rem_0rem_rgb(0_0_0_/0.18)] lg:[text-shadow:0.4rem_0.4rem_0rem_rgb(0_0_0_/0.18)] text-secondaryYellow font-bold text-left">Experience</h1>
             <div className="flex-col md:flex-row flex w-full mt-2 gap-10 space-y-15">
-              <Education/>
-              <Skills/>
+              <Education />
+              <Skills />
             </div>
           </div>
         </section>
@@ -110,14 +110,14 @@ export default function Home() {
             marginTop: `${margins[3] || 0}px`,
             marginBottom: `${margins[3] || 0}px`,
           }}
-          className="pb-[10vh] md:mx-0 mx-10"
+          className="pb-[10vh] md:mx-0 mx-0"
         >
-          <div className="flex flex-col justify-center mx-auto w-full px-4">
+          <div className="flex flex-col justify-center w-full px-4 md:px-8 2xl:px-12">
             <h1 className="text-[10rem] md:text-[8rem] lg:text-[6rem] [text-shadow:0.67rem_0.67rem_0rem_rgb(0_0_0_/0.18)] md:[text-shadow:0.53rem_0.53rem_0rem_rgb(0_0_0_/0.18)] lg:[text-shadow:0.4rem_0.4rem_0rem_rgb(0_0_0_/0.18)] text-secondaryYellow font-bold text-left mb-4">Projects</h1>
-            <div className="flex flex-row w-full justify-between space-x-6">
+            <div className=" flex w-full max-w-[95vw] flex-row justify-start gap-6 overflow-x-auto md:overflow-visible pb-2 snap-x snap-mandatory md:justify-between">
+              <ProjectCard title="Projectmap" description="A dashboard for visualizing and managing projects, specialized for the construction industry." imagePath="/images/Projectmap.png" tag="New!" />
               <ProjectCard title="Sabroso" description="Developing a smart restaurant ordering system. Orders are instantly sent to the kitchen or bar, streamlining the workflow and improving the overall dining experience." imagePath="/images/Sabroso.png" />
               {/* <ProjectCard title="Actipulse" description="Developing an application/device to improve productivity and health by tracking sitting-habits." imagePath="/images/Actipulse.png" /> */}
-              <ProjectCard title="Projectmap" description="A dashboard for visualizing and managing projects, specialized for the construction industry." imagePath="/images/Projectmap.png" tag="New!" />
               <ProjectCard title="WiebelToren" description="Designing and developing a game for the less-abled community, played with the Pillo from Hulan." imagePath="/images/WiebelToren.png" />
             </div>
           </div>
--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -5,18 +5,18 @@ import { useRouter } from "next/navigation";
 export default function ProjectCard({ title, description, imagePath, tag }: { title: string; description: string; imagePath: string; tag?: string }) {
   const router = useRouter();
   return (
-    <div className="relative bg-primaryYellow rounded-2xl w-108 h-126 flex flex-col p-4 shadow-[0.8rem_0.8rem_0_rgba(0,0,0,0.18)] hover:scale-105 hover:shadow-[1.8rem_1.8rem_0_rgba(0,0,0,0.18)] transition cursor-pointer"
+    <div className="relative shrink-0 bg-primaryYellow rounded-2xl w-[90vw] max-w-[70vw] aspect-[108/126] md:w-[28vw] md:max-w-[27rem] lg:w-[26vw] lg:max-w-[30rem] flex flex-col p-5 md:p-6 shadow-[0.8rem_0.8rem_0_rgba(0,0,0,0.18)] hover:scale-105 hover:shadow-[1.8rem_1.8rem_0_rgba(0,0,0,0.18)] transition cursor-pointer"
     onClick={() => router.push(`/${title.toLowerCase()}`)}
     >
-      <Image src={imagePath} alt={title} width={500} height={300} className="rounded-xl hover" />
-      <div className="flex flex-col mt-4 space-y-2">
-        <h3 className="text-[1.7rem] font-extrabold text-secondaryGreen">{title}</h3>
+      <Image src={imagePath} alt={title} width={800} height={450} className="rounded-xl w-full h-auto" />
+      <div className="flex flex-col mt-3 space-y-1 flex-1">
+        <h3 className="text-[4.4rem] md:text-[2.25rem] font-extrabold text-secondaryGreen">{title}</h3>
         {tag && (
-          <span className="absolute -top-1 -right-1 bg-red-500 text-white px-4 py-2 rounded-full text-[1.3rem] font-bold transform rotate-6 shadow-md">
+          <span className="absolute -top-1 -right-1 bg-red-500 text-white px-4 py-2 rounded-full text-[2rem] md:text-[1.4rem] font-bold transform rotate-6 shadow-md">
             {tag}
           </span>
         )}
-        <p className="text-[1.1rem] text-secondaryGreen leading-[1.7rem] line-clamp-5">{description}</p>
+        <p className="text-[2.1rem] md:text-[1.25rem] text-secondaryGreen leading-[2.4rem] md:leading-[1.95rem] line-clamp-5">{description}</p>
       </div>
       <ViewDetailsButton path={title} />
     </div>
